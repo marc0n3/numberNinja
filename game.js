4b5bcbf6d392e3d9de7eca8a4513fa13f6b1fa3c
nava.marco@gmail.com <nava.marco@gmail.com>
2021-05-15 08:28:27 +0200

    update

--- game.js
+++ game.js
@@ -1,7 +1,19 @@
 var startTime = new Date().getTime()
 
+function inizializza(level){
+    var numberOfItems = Math.pow(level+2,2);
+
+    for( var index = 0 ; index < numberOfItems; index++){
+        var numberEl = $("<li>1</li>");
+
+        $(".numbers").append(numberEl)
+    }
+}
+
 function onReady(){
    setInterval(orologio, 500);
+
+   inizializza(1);
 }
 
 function orologio(){
